6f55c33028def889cee1229716fff35b456c9dc4
Vadim Ivanov <ivanovvvadim@gmail.com>
2015-04-07 11:23:18 +0700

    reverse waypoint in directions

--- app/scripts/search/controller/searchDirectionsController.js
+++ app/scripts/search/controller/searchDirectionsController.js
@@ -7,7 +7,21 @@ searchDirectionsController.$inject = [
 function searchDirectionsController(SearchService, $location, MapService, Enums) {
 
   this.data = {
-    maxWaypoints: 5
+    maxWaypoints: 5,
+    modes: [
+      {
+        title: 'by Car',
+        value: 'car'
+      },
+      {
+        title: 'by Public Transport',
+        value: 'publicTransport'
+      },
+      {
+        title: 'by Walkinkg',
+        value: ''
+      }
+    ]
   };
 
   this.waypoints = [];
@@ -29,8 +43,16 @@ function searchDirectionsController(SearchService, $location, MapService, Enums)
 
   this.addWaypoint = function addWaypoint() {
     if (this.waypoints.length < this.data.maxWaypoints) {
-      this.waypoints.splice(this.waypoints.length - 2, 1, {});
+      this.waypoints.splice(this.waypoints.length - 1, 0, {});
     }
+  };
+  this.removeWaypoint = function removeWaypoint(index) {
+    if (index > 0 && index < this.waypoints.length - 1) {
+      this.waypoints.splice(index, 1);
+    }
+  };
+  this.reverseDirection = function reverseDirection() {
+    this.waypoints.reverse();
   };
 }
 module.exports = searchDirectionsController;
